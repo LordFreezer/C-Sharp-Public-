13726592889c4847b41209e9216cd0006da41c32
Lord Freeza <34351071+LordFreezer@users.noreply.github.com>
2020-08-28 15:29:45 -0500

    Chapter 1 almost complete

---
+++ BurmaShave/BurmaShave/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("One pound 85 cents");
+            Console.WriteLine("Half pound 50 cents");
+            Console.WriteLine("Big tube 35 cen ts");
+            Console.WriteLine("Don't put it off"); 
+            Console.WriteLine("Put it on");
+            Console.ReadLine();
+        }
+    }
+}
+
