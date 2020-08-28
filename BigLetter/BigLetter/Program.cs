bd901d1ae63997dc054d81d9c2ca9a8473a4333e
Lord Freeza <34351071+LordFreezer@users.noreply.github.com>
2020-08-28 15:20:43 -0500

    added BigLetter as a test

---
+++ BigLetter/BigLetter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class TakingTheBigH
+{ 
+    static void Main()
+    { 
+        Console.WriteLine("H      H");          //
+        Console.WriteLine("H      H");
+        Console.WriteLine("H      H");
+        Console.WriteLine("HHHHHHHH");
+        Console.WriteLine("H      H");
+        Console.WriteLine("H      H");
+        Console.WriteLine("H      H");
+        Console.ReadLine();
+    }
+}
